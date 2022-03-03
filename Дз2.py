9b1f6b385dcdc201932ffe42fbb7ca051b477958
Tony <dinekb96@mail.ru>
2022-03-03 12:10:34 +0500

    Dz 1 is done

--- Дз2.py
+++ Дз2.py
@@ -1,1 +1,6 @@
-print('hello')
+user_time = int(input('Enter time in seconds: '))
+print('Вы ввели: ', user_time, 'секунд')
+hours = user_time // 3600
+minutes = (user_time - hours * 3600) // 60
+seconds = user_time - (hours * 3600 + minutes * 60)
+print("Время в формате чч:мм:сс   {} : {} : {}".format(hours, minutes, seconds))
