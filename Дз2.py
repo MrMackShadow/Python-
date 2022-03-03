df2c7e852cb732c88aa0caac65c535efa1020aa7
Tony <dinekb96@mail.ru>
2022-03-03 12:11:33 +0500

    Dz 2 is done

--- Дз2.py
+++ Дз2.py
@@ -3,4 +3,4 @@ print('Вы ввели: ', user_time, 'секунд')
 hours = user_time // 3600
 minutes = (user_time - hours * 3600) // 60
 seconds = user_time - (hours * 3600 + minutes * 60)
-print("Время в формате чч:мм:сс   {} : {} : {}".format(hours, minutes, seconds))
+print("Время:   {} : {} : {}".format(hours, minutes, seconds))
