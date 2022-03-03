c01cbe866bfc61a2248d6595c1f43a807ff9b1a5
Tony <dinekb96@mail.ru>
2022-03-03 11:56:26 +0500

    Dz 1 is done

--- Дз.py
+++ Дз.py
@@ -1,29 +1,5 @@
-# name = input('Enter your name: ')
-# print('Hi', name, '!')
-
-# or_pass = '123456'
-# password = input('Enter color: ')
-#
-# if password == 'red':
-#     print('234')
-# elif color == 'black':
-#     print('123')
-# else:
-#     print('ok')
-
-# num = int(input('Enter a number 0-9: '))
-#
-# while num <= 10:
-#     print(num)
-#     num += 1
-
-i = 0
-
-while True:
-    i += 1
-    if i >= 10:
-        break
-    if i % 2 == 0:
-        continue
-    print('Круть')
-
+name = input('Hello please Enter your name: ')
+age = int(input('Enter your age: '))
+print('Your name is', name, 'and age is ', age)
+print(type(name))
+print(type(age))
