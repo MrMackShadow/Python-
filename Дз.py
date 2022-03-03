34693d5bd7e015e1eab5653df12bcc774be42f2e
Tony <dinekb96@mail.ru>
2022-03-03 09:46:28 +0500

    Task 1

---
+++ Дз.py
@@ -0,0 +1,28 @@
+# name = input('Enter your name: ')
+# print('Hi', name, '!')
+
+# or_pass = '123456'
+# password = input('Enter color: ')
+#
+# if password == 'red':
+#     print('234')
+# elif color == 'black':
+#     print('123')
+# else:
+#     print('ok')
+
+# num = int(input('Enter a number 0-9: '))
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+i = 0
+
+while True:
+    i += 1
+    if i >= 10:
+        break
+    if i % 2 == 0:
+        continue
+    print(i)
